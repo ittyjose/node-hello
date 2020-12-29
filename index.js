a984349f7aeb998f31543e4fc858f5bacb7dc222
Itty Jose <ittyjose@gmail.com>
2020-12-29 22:00:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   let msg = 'Hello Node!\n'
+  https.get('https://www.metaweather.com/api/location/44418/', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    msg=JSON.parse(data).explanation);
+      res.end(msg);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
   //axios.get('https://www.metaweather.com/api/location/44418/')
   //.then(response => {
   // msg = msg+ response;
@@ -15,7 +32,7 @@ const server = http.createServer((req, res) => {
  // .catch(error => {
  //   console.log(error);
  // });
-  res.end(msg);
+
   
 });
 
