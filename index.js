f8bec516e792a5ef1832ed1b85f02ec341f8affd
Itty Jose <ittyjose@gmail.com>
2020-12-29 22:51:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const http = require('http');
 const https = require('https');
-//const axios = require('axios');
 
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  let msg = 'Hello Node!\n'
-  https.get('http://www.metaweather.com/api/location/44418/', (resp) => {
-  let data = '';
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    msg=JSON.parse(data));
-      res.end(msg);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+getweather().then((response)=>{
+  let msg=JSON.parse(response).consolidated_weather[0].the_temp
+  msg=JSON.stringify(msg)+' C';
+  res.end(msg);
 });
-  //axios.get('https://www.metaweather.com/api/location/44418/')
-  //.then(response => {
-  // msg = msg+ response;
    
- // })
- // .catch(error => {
- //   console.log(error);
- // });
-
-    res.end(msg);
 });
 
+function getweather(){
+  return new Promise((resolve,reject)=>{
+  let msg;
+  https.get('https://www.metaweather.com/api/location/44418/', (resp) => {
+    let data = '';
+  
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      msg=data;
+    resolve (msg)
+    });
+  
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+})
+}
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
